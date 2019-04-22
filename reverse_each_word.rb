e3484cd62230743775630793d7470d0aca6cc979
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-22 15:19:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,18 +5,15 @@ def reverse_each_word(phrase)
     word.reverse
     new_array << word.reverse
   end
-  new_string = new_array.join(" ")
+  new_array.join(" ")
   
 end  
 
 def reverse_each_word(phrase)
   array = phrase.split
-  new_array = []
-  array.collect do |word|
-    word.reverse
-    new_array << word.reverse
-  end
-  new_string = new_array.join(" ")
+  array.collect {|word| word.reverse}
+  .join(" ")
+  
 end
 
 
